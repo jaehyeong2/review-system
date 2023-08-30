f348e8e01daade2151e823397aff1c3869c9c070
jaehyeong2 <wogud1514@gmail.com>
2023-08-30 16:54:06 +0900

    feat: api/ common module init

---
+++ common/src/test/java/jjfactory/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
