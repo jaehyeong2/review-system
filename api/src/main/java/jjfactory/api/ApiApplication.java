265a2f2636f83f5224fed8ffa355b9ea8db1e9e1
jaehyeong2 <wogud1514@gmail.com>
2023-08-30 17:10:12 +0900

    feature: 빈 스캔 패키지 수정

--- api/src/main/java/jjfactory/api/ApiApplication.java
+++ api/src/main/java/jjfactory/api/ApiApplication.java
@@ -3,7 +3,9 @@ package jjfactory.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = { "jjfactory.api", "jjfactory.common"}
+)
 public class ApiApplication {
 
 	public static void main(String[] args) {
