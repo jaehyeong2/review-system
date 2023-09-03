c475e14ad05546bf49e27d26690d7fd04d2943cc
jaehyeong2 <wogud1514@gmail.com>
2023-09-03 14:22:49 +0900

    common-feature: common response 추가

---
+++ common/src/main/java/jjfactory/common/response/CommonResponse.java
@@ -0,0 +1,26 @@
+package jjfactory.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class CommonResponse<T> {
+    private int code;
+    private T data;
+
+    public CommonResponse(T data) {
+        this.data = data;
+        this.code = HttpStatus.OK.value();
+    }
+
+    public static CommonResponse ok() {
+        return CommonResponse
+                .builder()
+                .code(HttpStatus.OK.value())
+                .build();
+    }
+}
