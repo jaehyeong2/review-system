0e0fa9f7fd1d5867a62712569a3d66c67fd1607a
jaehyeong2 <wogud1514@gmail.com>
2023-09-10 14:53:10 +0900

    common feature: team / organization entity에 분기정보 컬럼 추가

--- common/src/main/java/jjfactory/common/organization/domain/Organization.java
+++ common/src/main/java/jjfactory/common/organization/domain/Organization.java
@@ -1,20 +1,25 @@
 package jjfactory.common.organization.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
+@Table(
+        indexes = @Index(columnList = "yearQuarterId")
+)
 public class Organization {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private Long yearQuarterId;
     private String name;
     private String code;
     @CreationTimestamp
--- common/src/main/java/jjfactory/common/organization/domain/team/Team.java
+++ common/src/main/java/jjfactory/common/organization/domain/team/Team.java
@@ -2,18 +2,27 @@ package jjfactory.common.organization.domain.team;
 
 import jakarta.persistence.*;
 import jjfactory.common.organization.domain.Organization;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
+@Table(
+        indexes = @Index(columnList = "yearQuarterId")
+)
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long yearQuarterId;
     private String name;
-
+    private String code;
     @ManyToOne(fetch = FetchType.LAZY)
     private Organization organization;
 
